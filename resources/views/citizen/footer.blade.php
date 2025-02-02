12428fb39c0b2bc37355f3193e5088507e8b8997
i'mac Amana <hissro@hotmail.com>
2025-02-02 16:50:48 +0300

    rebuild

--- resources/views/citizen/footer.blade.php
+++ resources/views/citizen/footer.blade.php
@@ -33,7 +33,7 @@
 
 <!-- Vendor JS Files -->
 <script src="{{ asset('css/front/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('css/front/assets/assets/vendor/php-email-form/validate.js') }}"></script>
+<script src="{{ asset('css/front/assets/vendor/php-email-form/validate.js') }}"></script>
 <script src="{{ asset('css/front/assets/vendor/aos/aos.js') }}"></script>
 <script src="{{ asset('css/front/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
 <script src="{{ asset('css/front/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
--- resources/views/citizen/home.blade.php
+++ resources/views/citizen/home.blade.php
@@ -1,44 +1,4 @@
-{{-- @extends('citizen.header') --}}
-{{-- @extends('citizen.footer') --}}
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title> سفارة جمهورية السودان في المملكة العربية السعودية </title>
-  <meta name="description" content="">
-  <meta name="keywords" content="">
-
-  <!-- Favicons -->
-  <link href="{{ asset('css/front/assets/img/BottomLogo.jpg') }}" rel="icon">
-  <link href="{{ asset('css/front/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
-
-  <!-- Fonts -->
-  <link href="https://fonts.googleapis.com" rel="preconnect">
-  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="{{ asset('css/front/assets/vendor/bootstrap/css/bootstrap.rtl.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/front/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/front/assets/vendor/aos/aos.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/front/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/front/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-
-  <!-- Main CSS File -->
-  <link href="{{ asset('css/front/assets/css/main.css') }}" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: Reveal
-  * Template URL: https://bootstrapmade.com/reveal-bootstrap-corporate-template/
-  * Updated: Aug 07 2024 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
-</head>
-
+@extends('citizen.header')
 <body class="index-page" dir="rtl">
 
   <header id="header" class="header sticky-top">
@@ -475,51 +435,4 @@
 
  
   </main>
-
-  <footer id="footer" class="footer dark-background">
-    <div class="container">
-      <h3 class="sitename">Reveal</h3>
-      <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
-      <div class="social-links d-flex justify-content-center">
-        <a href=""><i class="bi bi-twitter-x"></i></a>
-        <a href=""><i class="bi bi-facebook"></i></a>
-        <a href=""><i class="bi bi-instagram"></i></a>
-        <a href=""><i class="bi bi-skype"></i></a>
-        <a href=""><i class="bi bi-linkedin"></i></a>
-      </div>
-      <div class="container">
-        <div class="copyright">
-          <span>Copyright</span> <strong class="px-1 sitename">Reveal</strong> <span>All Rights Reserved</span>
-        </div>
-        <div class="credits">
-          <!-- All the links in the footer should remain intact. -->
-          <!-- You can delete the links only if you've purchased the pro version. -->
-          <!-- Licensing information: https://bootstrapmade.com/license/ -->
-          <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-        </div>
-      </div>
-    </div>
-  </footer>
-
-  <!-- Scroll Top -->
-  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Preloader -->
-  <div id="preloader"></div>
-
-  <!-- Vendor JS Files -->
-  <script src="{{ asset('css/front/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('css/front/assets/assets/vendor/php-email-form/validate.js') }}"></script>
-  <script src="{{ asset('css/front/assets/vendor/aos/aos.js') }}"></script>
-  <script src="{{ asset('css/front/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
-  <script src="{{ asset('css/front/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
-  <script src="{{ asset('css/front/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
-  <script src="{{ asset('css/front/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
-
-  <!-- Main JS File -->
-  <script src="{{ asset('css/front/assets/js/main.js') }}"></script>
-
-</body>
-
-</html>
+  @extends('citizen.footer')
